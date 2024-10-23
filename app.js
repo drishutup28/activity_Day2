9258b7ff19fa9b9982a382b3ecd13fb67af3858a
drishutup28 <123671272+drishutup28@users.noreply.github.com>
2024-10-23 11:37:48 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -1,27 +1,35 @@
-// to do 
+
 const express = require("express");
 const app = express();
 const path = require("path");
-const PORT = 8080;
+const PORT = 8000;
 app.set('view engine', 'ejs');
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-let tasks = [];
-app.get("/", (req, res) => {
-    let name = "Sam";
-    let place = "Jakhu";
-    let currentHour = new Date().getHours();
-    let greeting;
 
-    if (currentHour < 12) {
-        greeting = "Good Morning";
-    } else if (currentHour < 18) {
-        greeting = "Good Afternoon";
-    } else {
-        greeting = "Good Evening";
-    }
-    res.render("index", { name, destination: place, greeting, tasks });
+let tasks = [];
+let products = [
+    { "id": 1, "name": "TV", "price": 45000 },
+    { "id": 2, "name": "Iphone", "price": 78000 },
+    { "id": 3, "name": "AC", "price": 32000 },
+    { "id": 4, "name": "Monitor", "price": 24440 },
+    { "id": 5, "name": "Ipad", "price": 68000 },
+];
+
+function time() {
+    const hour = new Date().getHours();
+    if (hour < 12) return "Good Morning";
+    else if (hour < 15) return "Good Afternoon";
+    else if (hour < 20) return "Good Evening";
+    else return "Good Night";
+}
+
+app.get("/welcome", (req, res) => {
+    let name = "John";
+    let good = time();
+    res.render('welcome', { name, good });
 });
+
 app.post("/add-task", (req, res) => {
     const newTask = req.body.task;
     if (newTask) {
@@ -29,87 +37,35 @@ app.post("/add-task", (req, res) => {
     }
     res.redirect("/todo");
 });
-app.post("/edit-task/:id", (req, res) => {
-    const taskId = req.params.id;
-    const updatedTask = req.body.updatedTask;
-    if (updatedTask && tasks[taskId]) {
-        tasks[taskId] = updatedTask;
-    }
-    res.redirect("/todo");
-});
+
 app.post("/delete-task/:id", (req, res) => {
-    const taskId = req.params.id;
-    tasks.splice(taskId, 1);
+    const taskId = parseInt(req.params.id);
+    if (!isNaN(taskId) && taskId >= 0 && taskId < tasks.length) {
+        tasks.splice(taskId, 1);
+    }
     res.redirect("/todo");
 });
-app.get("/todo", (req, res) => {
-    let name = "Sam";
-    let place = "Jakhu";
-    let currentHour = new Date().getHours();
-    let greeting;
 
-    if (currentHour < 12) {
-        greeting = "Good Morning";
-    } else if (currentHour < 18) {
-        greeting = "Good Afternoon";
-    } else {
-        greeting = "Good Evening";
+app.get("/todo", (req, res) => {
+    res.render('todo', { tasks });
+});
+
+app.get("/products", (req, res) => {
+    const searchQuery = req.query.search;
+    let productSearch = products;
+
+    if (searchQuery) {
+        productSearch = products.filter(product => 
+            product.name.toLowerCase().includes(searchQuery.toLowerCase())
+        );
     }
 
-    res.render("index", { name, destination: place, greeting, tasks });
+    res.render('products', { products: productSearch });
 });
 
 app.listen(PORT, (err) => {
     if (err) console.log(err);
-    else console.log(Listening to Port ${PORT});
+    else console.log(`Listening to Port ${PORT}`);
 });
 
-// welcome 
-// const express = require("express");
-// const app = express();
-// const path = require("path");
-// const PORT = 8080;
-// app.set('view engine', 'ejs');
-// app.use(express.json());
-
-// filepath = path.join(__dirname, "/views/index.ejs");
-// // app.get("/", (req, res)=>{
-// //     let name = "Sam";
-// //     let place = "Hyderabad";
-// //     res.render(filepath, {name, destination:place});
-// // })
-
-// function time(){
-//     const hour = new Date().getHours();
-//     if(hour < 12) return "Good Morning";
-//     else if(hour < 15) return "Good Afternoon";
-//     else if(hour < 15 && hour >= 12) return "Good Afternoon";
-//     else if(hour < 20 && hour >= 16) return "Good Evening";
-//     else return "Good Night";
-// }
-// app.get("/welcome", (req, res)=>{
-//     let name = "John";
-//     let good = time();
-//     // let place = "Hyderabad";
-//     res.render(filepath, {name, good});
-// })
-
-// app.listen(PORT, (err)=>{
-//     if(err) console.log(err);
-//     else console.log(Listening to Port ${PORT});
-// })
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+ghjk
